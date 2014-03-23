0b58ace092879f6cb22568282b7540e1eb22b7ab
Joseph Utecht <thenmal@gmail.com>
2014-03-22 23:00:53 -0500

    added first view

--- content/views.py
+++ content/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from content.models import *
 
 # Create your views here.
+def index(request):
+    recent_news = News.objects.all()
+    context = {'news': recent_news}
+    return render(request, 'index.html', context)
--- ffxiv_pulse/urls.py
+++ ffxiv_pulse/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ffxiv_pulse.views.home', name='home'),
+    url(r'^$', 'content.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
