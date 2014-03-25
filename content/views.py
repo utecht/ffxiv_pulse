0ee48dd89973005dbb6ee9b5635cd620f5a48bd7
Joseph Utecht <thenmal@gmail.com>
2014-03-25 17:06:02 -0500

    added news page

--- content/views.py
+++ content/views.py
@@ -1,8 +1,21 @@
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from content.models import *
 
 # Create your views here.
 def index(request):
     recent_news = News.objects.all().order_by('-publish_date')
-    context = {'news': recent_news}
+    updates = []
+    for u in News.objects.all():
+        updates.append({'name':u.title,
+                        'url':reverse('news', kwargs={'news_id':u.id}),
+                        'date':u.publish_date})
+
+    context = {'news': recent_news, 'updates':updates}
     return render(request, 'index.html', context)
+
+
+def news(request, news_id):
+    n = News.objects.get(id=news_id)
+    context = {'n': n}
+    return render(request, 'news.html', context)
--- ffxiv_pulse/urls.py
+++ ffxiv_pulse/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'content.views.index', name='index'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^news/(?P<news_id>\d+)', 'content.views.news', name='news'),
     url(r'^admin/', include(admin.site.urls)),
 )
